ede2b63c56de41154a0bc84a6eead1e0106711e8
Kunal <kunals1994@gmail.com>
2016-04-11 17:26:26 -0400

    add models and modify installed_apps

--- step_1_set_up_db/blog/blog_content/models.py
+++ step_1_set_up_db/blog/blog_content/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length = 200)
+    date = models.DateTimeField(auto_now = True)
+    content = models.TextField()
+
+    def __str__(self):
+        return ("Blog Post: " + self.title)
+
+class Comment(models.Model):
+    article_id = models.IntegerField()
+    date = models.DateTimeField(auto_now = True)
+    name = models.CharField(max_length = 100)
+    content = models.TextField()
+    
+    def __str__(self):
+        return ("Comment by: " + self.name)
